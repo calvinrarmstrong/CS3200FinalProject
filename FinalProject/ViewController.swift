c53c02e0b4cf03941b41175b9366ef30015bb791
Calvin Armstrong <calvinarmstrong@Calvins-MacBook-Pro.local>
2017-03-16 20:56:36 -0600

    Initial Commit

---
+++ FinalProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FinalProject
+//
+//  Created by Calvin Armstrong on 3/16/17.
+//  Copyright © 2017 Calvin Armstrong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
